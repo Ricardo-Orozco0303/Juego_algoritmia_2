699f0a61880d034872a9cd463ad856e411e3a950
Ricardo.orozco0303 <141371507+Ricardo-Orozco0303@users.noreply.github.com>
2025-04-28 14:30:41 -0500

    Add files via upload

---
+++ Juego de A_Y_p_II/src/juego/de/a_y_p_ii/JuegoDeA_Y_p_II.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package juego.de.a_y_p_ii;
+
+/**
+ *
+ * @author Ricar
+ */
+public class JuegoDeA_Y_p_II {
+
+    public static void main(String[] args) {
+        Juego Inicio_1 = new Juego();
+        Inicio_1.setSize(1080, 650); // Tamaño de la ventana
+        Inicio_1.setLocationRelativeTo(null); // Centrar en pantalla
+        Inicio_1.setVisible(true);
+
+    }
+
+}
